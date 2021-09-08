c043215b60983b8474f987cbef57fb7daa8e9a51
joisfe <rhsnqk@gmail.com>
2021-09-08 23:41:20 +0900

    repair

---
+++ _pages/about.md
@@ -0,0 +1,10 @@
+---
+title: "나의 소개"
+layout: single
+permalink: /about/
+author_profile: true
+---
+
+안녕하세요 반가우요
+
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "딥러닝이란",
-        "excerpt":" ","categories": ["Edge Case"],
-        "tags": ["edge case","layout = single","title = \"딥러닝의 시작\""],
-        "url": "/blog/edge%20case/%EB%94%A5%EB%9F%AC%EB%8B%9D%EC%9D%B4%EB%9E%80/",
+        "title": "Edge Case: Nested and Mixed Lists",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that      Lists within lists do not break the ordered list numbering order   Your list styles go deep enough.   Ordered – Unordered – Ordered      ordered item   ordered item            unordered       unordered                    ordered item           ordered item                           ordered item   ordered item   Ordered – Unordered – Unordered      ordered item   ordered item            unordered       unordered                    unordered item           unordered item                           ordered item   ordered item   Unordered – Ordered – Unordered      unordered item   unordered item            ordered       ordered                    unordered item           unordered item                           unordered item   unordered item   Unordered – Unordered – Ordered      unordered item   unordered item            unordered       unordered                    ordered item           ordered item                           unordered item   unordered item   Task Lists      Finish my changes   Push my commits to GitHub   Open a pull request  ","categories": ["Deep_Learning"],
+        "tags": ["hew"],
+        "url": "/deep_learning/hihi/",
         "teaser": null
       }]
